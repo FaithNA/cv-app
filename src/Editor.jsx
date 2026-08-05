a57163914669badd131ff993e07cefaa0c8670b2
Faith <faithakanga2404@gmail.com>
2026-08-05 06:19:17 +0100

    Creation of PersonalInfo, Eduation and Experience components

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,8 +1,9 @@
+import PersonalInfo from "./PersonalInfo.jsx"
 export default function Editor(){
     return (
         
             <div className="Editor m-2 min-h-screen grid grid-row-3 gap-2">
-                <div className="sec border-1"></div>
+                <PersonalInfo/>
                 <div className="sec border-1"></div>
                 <div className="sec border-1"></div>
             </div>
---
+++ src/PersonalInfo.jsx
@@ -0,0 +1,10 @@
+export default function PersonalInfo(){
+    return(
+        <div>
+            <form action="GET">
+                <label htmlFor="names">Full Names</label>
+                <input type="text" placeholder="Jon Doe"/>
+            </form>
+        </div>
+    )
+}
