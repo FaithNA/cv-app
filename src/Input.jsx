d691a4593d4d971a7035c65d39db4df5b2796297
Faith <faithakanga2404@gmail.com>
2026-08-07 21:01:52 +0100

    Creation of Components for Button and Input

---
+++ src/Input.jsx
@@ -0,0 +1,10 @@
+export default function Input(){
+    return (
+        <>
+            <label htmlFor="
+            ">
+                <input type="text" />
+            </label>
+        </>
+    )
+}
